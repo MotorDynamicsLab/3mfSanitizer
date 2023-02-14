1b6072f7f5788f68bbce5d62916143517c4a3b95
David Sun <cneshi@gmail.com>
2023-02-14 17:05:05 +0800

    Initial commit of project files

---
+++ 3mfSantizer/ConfigSanitizer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.IO.Compression;
+
+
+namespace _3mfSantizer
+{
+    /// <summary>
+    /// Opens a 3MF file and removes any config files stored in the metadata
+    /// </summary>
+    public class ConfigSanitizer
+    {
+        /// <summary>
+        /// Sanitizes the provided 3MF file in place (old file is overwritten)
+        /// </summary>
+        /// <param name="filePath">Path of the file to sanitize</param>
+        /// <returns>Number of config items removed from 3MF archive</returns>
+        public static int SanitizeInPlace(string filePath)
+        {
+            int numConfigsRemoved = 0;
+            filePath = Path.GetFullPath(filePath);
+            ZipArchive archive = ZipFile.Open(filePath, ZipArchiveMode.Update);
+            List<ZipArchiveEntry> deletionList = new List<ZipArchiveEntry>();
+
+            //Build a list of files to delete from the archive
+            foreach (ZipArchiveEntry entry in archive.Entries)
+            {
+                if (entry.FullName.EndsWith(".config", StringComparison.OrdinalIgnoreCase) &&
+                    entry.FullName.StartsWith("Metadata/") )
+                {
+                    deletionList.Add(entry);
+                }
+            }
+
+            //Delete all the files in the list
+            foreach (ZipArchiveEntry entry in deletionList)
+            {
+                entry.Delete();
+                ++numConfigsRemoved;
+            }
+
+            archive.Dispose();
+            return numConfigsRemoved;
+        }
+    }
+}
---
+++ 3mfSantizer/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _3mfSantizer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void FileSelect(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.DefaultExt = ".3mf";
+            dlg.Filter = "3MF files (*.3mf)|*.3mf";
+            if (true == dlg.ShowDialog())
+            {
+                int configsRemoved = ConfigSanitizer.SanitizeInPlace(dlg.FileName);
+                string completeMessage = string.Format("Sanitation Complete! {0} config files removed.", configsRemoved);
+                MessageBox.Show(completeMessage, "Sanitation Complete!", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        private void FolderSelect(object sender, RoutedEventArgs e)
+        {
+            using (var dialog = new System.Windows.Forms.FolderBrowserDialog())
+            {
+                System.Windows.Forms.DialogResult result = dialog.ShowDialog();
+                
+                if (result == System.Windows.Forms.DialogResult.OK)
+                {
+                    int filesSanitized = 0;
+
+                    foreach (string file in Directory.GetFiles(dialog.SelectedPath,"*3mf",SearchOption.AllDirectories))
+                    {
+                        int configsRemoved = ConfigSanitizer.SanitizeInPlace(file);
+                        if (configsRemoved > 0) filesSanitized += 1;
+                    }
+
+                    string completeMessage = string.Format("Sanitation Complete! {0} 3mf files were sanitized.", filesSanitized);
+                    MessageBox.Show(completeMessage, "Sanitation Complete!", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
+        }
+    }
+}
